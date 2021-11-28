54a5821b480532c66a7be7c87d8f8eb0b9850b57
Michail Kasatov <kasatovmisha@gmail.com>
2021-11-28 12:23:36 +0300

    Улучшил увеличение/уменьшение сетки. Примечание - сетка дергается при увеличении

--- Interface/main.py
+++ Interface/main.py
@@ -1,13 +1,19 @@
 import pygame
 
+import Grid
+
 SIZES = WIDTH, HEIGHT = (1200, 800)
-tile = 10
 FPS = 30
+
+CELL = 100  # size of cell
+X0, Y0 = 0, 0  # origin of coordinates
 
 pygame.init()
 screen = pygame.display.set_mode(SIZES, pygame.RESIZABLE)
 pygame.display.set_caption("Life")
 clock = pygame.time.Clock()
+
+grid = Grid.Grid(x0=X0, y0=Y0, cell=CELL)
 
 while True:
     screen.fill(pygame.Color('black'))
@@ -15,25 +21,20 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 4:
-                tile *= 1.1
-            if event.button == 5:
-                tile /= 1.1
+                grid.increase(*event.pos)
+            elif event.button == 5:
+                grid.decrease(*event.pos)
 
     # get width and height of window
     width, height = screen.get_size()
 
     # draw grid
-    for x in range(0, width, int(tile)):
+    for x in range(grid.x1, width, grid.cell):
         pygame.draw.line(screen, pygame.Color('dimgray'), (x, 0), (x, height))
-    for y in range(0, height, int(tile)):
+    for y in range(grid.y1, height, grid.cell):
         pygame.draw.line(screen, pygame.Color('dimgray'), (0, y), (width, y))
-
-    # change size of window
-
-    # change scale of grid
 
     pygame.display.flip()
     clock.tick(FPS)
